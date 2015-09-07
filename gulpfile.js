cc2fa7d03b76628abe41d67de95c82a789c2d010
Bucky Schwarz <d.w.schwarz@gmail.com>
2015-09-06 21:53:33 -0400

    first passing dumb test

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,30 @@
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
+
 var jshint = require('gulp-jshint');
 var browserify = require('browserify');
+var tape = require('gulp-tape');
 
 var paths = {
-    'js_source': 'src/js/',
-    'js_dist': 'dist/',
-    'js_test': 'src/spec/',
-    'react_source': 'src/js/views/',
-    'react_dist': 'dist/views'
+    'js_source': 'js/src/',
+    'js_test': 'js/test/',
+    'js_dist': 'dist/'
 };
 
 gulp.task('lint', function() {
-    return gulp.src([paths.js_source + '*.js', paths.js_test + '*.js', 'gulpfile.js'])
+    return gulp.src([
+            paths.js_source + '*.js',
+            paths.js_test + '*.js',
+            'gulpfile.js'
+        ])
         .pipe(jshint())
         .pipe(jshint.reporter('default', { verbose: true }))
         .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', function() {
+    return gulp.src(paths.js_test + '/*.js')
+        .pipe(tape());
 });
 
 gulp.task('build', function() {
---
+++ js/test/graph.js
@@ -0,0 +1,10 @@
+var tape = require('tape');
+
+tape("yeah", function(t) {
+    t.plan(1);
+    console.log('leo dicaprio');
+
+    t.true(true);
+
+    t.end();
+});
