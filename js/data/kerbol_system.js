a0f3b8ef35beb331d6420575575997e0056afdd2
Bucky Schwarz <d.w.schwarz@gmail.com>
2015-09-12 14:20:57 -0400

    moved kerbol system data to external file

---
+++ js/data/kerbol_system.js
@@ -0,0 +1,32 @@
+var Node = require('../src/node');
+var Edge = require('../src/edge');
+
+var newNode = function(id) {
+    var node = new Node();
+    node.id = id;
+
+    return node;
+};
+
+var newEdge = function(options) {
+    return new Edge(options.deltav, options.name);
+};
+
+// values taken from http://i.imgur.com/duY2S.png
+var nodes = {
+    kerbin: newNode('Kerbin'),
+    low_kerbin_orbit: newNode('Low Kerbin Orbit'),
+    geostationary_transfer_orbit: newNode('GTO'),
+    mun_transfer: newNode('Mun Transfer')
+};
+
+var edges = {
+    kerbin_lko: newEdge({ deltav: 3800, name: 'kerbin-lko' }),
+    lko_gto: newEdge({ deltav: 670, name: 'lko-gto' }),
+    lko_mun_transfer: newEdge({ deltav: 190, name: 'lko-mun_transfer' })
+};
+
+// es6 !!
+module.exports = {
+    nodes, edges
+};
--- js/test/graph.js
+++ js/test/graph.js
@@ -4,6 +4,7 @@ var Node = require('../src/node');
 var Edge = require('../src/edge');
 
 var solar_system = require('../data/solar_system');
+var kerbol_system = require('../data/kerbol_system');
 
 var newNode = function(id) {
     var node = new Node();
@@ -22,26 +23,16 @@ describe("walking the graph", function(t) {
     describe("walking a simple linear graph", function(t) {
         t.plan(5);
 
-        // values taken from http://i.imgur.com/duY2S.png
-        var kerbin = newNode('Kerbin');
-        var low_kerbin_orbit = newNode('Low Kerbin Orbit');
-        var geostationary_transfer_orbit = newNode('GTO');
-        var mun_transfer = newNode('Mun Transfer');
+        graph.addEdge(kerbol_system.edges.kerbin_lko, kerbol_system.nodes.kerbin, kerbol_system.nodes.low_kerbin_orbit);
+        graph.addEdge(kerbol_system.edges.lko_gto, kerbol_system.nodes.low_kerbin_orbit, kerbol_system.nodes.geostationary_transfer_orbit);
+        graph.addEdge(kerbol_system.edges.lko_mun_transfer, kerbol_system.nodes.low_kerbin_orbit, kerbol_system.nodes.mun_transfer);
 
-        var kerbin_lko = newEdge({ deltav: 3800, name: 'kerbin-lko' });
-        var lko_gto = newEdge({ deltav: 670, name: 'lko-gto' });
-        var lko_mun_transfer = newEdge({ deltav: 190, name: 'lko-mun_transfer' });
+        t.true(graph.areAdjacent(kerbol_system.nodes.kerbin, kerbol_system.nodes.low_kerbin_orbit));
+        t.true(graph.areAdjacent(kerbol_system.nodes.low_kerbin_orbit, kerbol_system.nodes.geostationary_transfer_orbit));
+        t.true(graph.areAdjacent(kerbol_system.nodes.low_kerbin_orbit, kerbol_system.nodes.mun_transfer));
 
-        graph.addEdge(kerbin_lko, kerbin, low_kerbin_orbit);
-        graph.addEdge(lko_gto, low_kerbin_orbit, geostationary_transfer_orbit);
-        graph.addEdge(lko_mun_transfer, low_kerbin_orbit, mun_transfer);
-
-        t.true(graph.areAdjacent(kerbin, low_kerbin_orbit));
-        t.true(graph.areAdjacent(low_kerbin_orbit, geostationary_transfer_orbit));
-        t.true(graph.areAdjacent(low_kerbin_orbit, mun_transfer));
-
-        t.false(graph.areAdjacent(kerbin, geostationary_transfer_orbit));
-        t.false(graph.areAdjacent(kerbin, mun_transfer));
+        t.false(graph.areAdjacent(kerbol_system.nodes.kerbin, kerbol_system.nodes.geostationary_transfer_orbit));
+        t.false(graph.areAdjacent(kerbol_system.nodes.kerbin, kerbol_system.nodes.mun_transfer));
 
         t.end();
     });
