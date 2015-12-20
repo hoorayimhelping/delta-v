394e54f0053458aebbc74937fa9a49377a8cd5f8
Bucky Schwarz <d.w.schwarz@gmail.com>
2015-12-20 12:02:58 -0500

    whitespace changes, added editorconfig

--- gulpfile.js
+++ gulpfile.js
@@ -11,73 +11,72 @@ var shell = require('gulp-shell');
 var react = require('gulp-react');
 
 var paths = {
-    'js_source': {
-        graph: 'js/graph/',
-        maps: 'js/maps',
-        canvas: '/js/canvas'
-    },
-    'js_test': 'js/test/',
-    'js_dist': 'js/',
-    'react_source': 'js/views/',
-    'react_dist': 'js/views/'
+  'js_source': {
+    graph: 'js/graph/',
+    maps: 'js/maps',
+    canvas: '/js/canvas'
+  },
+  'js_test': 'js/test/',
+  'js_dist': 'js/',
+  'react_source': 'js/views/',
+  'react_dist': 'js/views/'
 };
 
 gulp.task('env-dev', function() {
-    process.env.NODE_ENV = 'development';
+  process.env.NODE_ENV = 'development';
 });
 
 gulp.task('env-prod', function() {
-    process.env.NODE_ENV = 'production';
+  process.env.NODE_ENV = 'production';
 });
 
 gulp.task('lint', function() {
-    return gulp.src([
-            paths.js_source.graph + '*.js',
-            paths.js_source.maps + '*.js',
-            paths.js_source.canvas + '*.js',
-            paths.js_test + '*.js',
-            'gulpfile.js'
-    ])
-    .pipe(jshint({ esnext: true }))
-    .pipe(jshint.reporter('default', { verbose: true }))
-    .pipe(jshint.reporter('fail'));
+  return gulp.src([
+    paths.js_source.graph + '*.js',
+    paths.js_source.maps + '*.js',
+    paths.js_source.canvas + '*.js',
+    paths.js_test + '*.js',
+    'gulpfile.js'
+  ])
+  .pipe(jshint({ esnext: true }))
+  .pipe(jshint.reporter('default', { verbose: true }))
+  .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('test', shell.task([
-    'tape ' + paths.js_test + '* | faucet',
+  'tape ' + paths.js_test + '* | faucet',
 ]));
 
 gulp.task('test-debug', function() {
-    return gulp.src(paths.js_test + '/*.js')
-        .pipe(tape());
+  return gulp.src(paths.js_test + '/*.js')
+    .pipe(tape());
 });
 
 gulp.task('transform', function() {
-    return gulp.src(paths.react_source + '*.jsx')
-        .pipe(react())
-        .pipe(gulp.dest(paths.react_dist));
+   return gulp.src(paths.react_source + '*.jsx')
+    .pipe(react())
+    .pipe(gulp.dest(paths.react_dist));
 });
 
 gulp.task('build', function() {
-    return browserify(paths.react_dist + 'main.js')
-        .transform(reactify)
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(gulp.dest(paths.js_dist));
+  return browserify(paths.react_dist + 'main.js')
+    .transform(reactify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest(paths.js_dist));
 });
 
 gulp.task('default', function() {
-    gulp.watch(
-        [
-            paths.react_source + '/*.jsx',
-            paths.js_source.graph + '*.js',
-            paths.js_source.maps + '*.js',
-            paths.js_source.canvas + '*.js'
-        ],
-        ['env-dev', 'transform', 'build']
-    ).on('error', function(error) {
-        console.log(error.toString());
-    });
+  gulp.watch([
+      paths.react_source + '/*.jsx',
+      paths.js_source.graph + '*.js',
+      paths.js_source.maps + '*.js',
+      paths.js_source.canvas + '*.js'
+    ],
+    ['env-dev', 'transform', 'build']
+  ).on('error', function(error) {
+    console.log(error.toString());
+  });
 });
 
 gulp.task('development', ['env-dev', 'lint', 'test', 'transform', 'build']);
--- js/graph/edge.js
+++ js/graph/edge.js
@@ -1,25 +1,25 @@
 var Edge = function(value, name) {
-    this.value = value;
-    this.name = name;
+  this.value = value;
+  this.name = name;
 
-    this.id = name + '-' + value;
-    this.nodes = {};
+  this.id = name + '-' + value;
+  this.nodes = {};
 };
 
 Edge.prototype = {
-    add: function(node1, node2) {
-        this.nodes = {};
+  add: function(node1, node2) {
+    this.nodes = {};
 
-        this.nodes = {
-            head: node1,
-            tail: node2
-        };
-    },
+    this.nodes = {
+      head: node1,
+      tail: node2
+    };
+  },
 
-    areAdjacent: function(node1, node2) {
-        return this.nodes.head.id === node1.id &&
-            this.nodes.tail.id === node2.id;
-    }
+  areAdjacent: function(node1, node2) {
+    return this.nodes.head.id === node1.id &&
+      this.nodes.tail.id === node2.id;
+  }
 };
 
 module.exports = Edge;
